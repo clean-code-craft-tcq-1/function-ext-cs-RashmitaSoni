6e9df62758f14d6f9cde08660c2f2742465f9b3b
RashmitaSoni <36473003+RashmitaSoni@users.noreply.github.com>
2021-03-10 14:47:30 +0530

    Update BatteryChargeRate.cs

--- BatteryChargeRate.cs
+++ BatteryChargeRate.cs
@@ -11,8 +11,8 @@ namespace BatteryCharging
         static float highWarningValue = (float)(highBreachValue - (highBreachValue * 0.05));
         public static void CheckChargeRate(float chargeRate_CRate)
         {
-            BatteryAbnormalConditions.CheckBreachingPoints(English.chargeRate, German.chargeRate,chargeRate_CRate, lowBreachValue, highBreachValue);
-            BatteryAbnormalConditions.CheckWarningPoints(English.chargeRate, German.chargeRate,chargeRate_CRate, lowBreachValue, lowWarningValue, highWarningValue, highBreachValue);
+            BatteryAbnormalConditions.CheckBreachingPoints(chargeRate_CRate, lowBreachValue, highBreachValue);
+            BatteryAbnormalConditions.CheckWarningPoints(chargeRate_CRate, lowBreachValue, lowWarningValue, highWarningValue, highBreachValue);
         }
     }
 }
