9667bc51529d5a8e3d6775c99fface909bce0411
RashmitaSoni <36473003+RashmitaSoni@users.noreply.github.com>
2021-03-10 22:13:15 +0530

    Update BatteryChargingTest.cs

--- BatteryChargingTest.cs
+++ BatteryChargingTest.cs
@@ -5,17 +5,24 @@ namespace BatteryCharging
 {
     public class BatteryChargingTest
     {
-       public static void BatteryChargingTestFactors(float chargingTemperature_Celsius, float stateOfCharge_percentage, float chargeRate_CRate)
+        public static void BatteryChargingTestFactors(float chargingTemperature_Celsius, float stateOfCharge_percentage, float chargeRate_CRate)
         {
             BatteryChargingFactors.CheckChargingTemperature(chargingTemperature_Celsius);
             BatteryChargingFactors.CheckStateOfCharge(stateOfCharge_percentage);
             BatteryChargingFactors.CheckChargeRate(chargeRate_CRate);
             DisplayBatteryStatus();
         }
-       public static void DisplayBatteryStatus()
+        public static void DisplayBatteryStatus()
         {
-            string BatteryStatus = (BatteryAbnormalConditions.GetBatteryFaultyStatus() == true) ? "Battery is faulty" : "Battery is OK";
-            Console.WriteLine(BatteryStatus);
+            if(BatteryAbnormalConditions.IsBatteryFaultyStatus())
+            {
+                Console.WriteLine("Battery is faulty\nBatterie ist defekt");
+            }
+            else
+            {
+                Console.WriteLine("Battery is OK\nBatterie ist in Ordnung");
+            }
+           
         }
     }
 }
