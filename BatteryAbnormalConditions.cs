7f097de1be2d8aec7fd1a284cf28cb74736dc609
RashmitaSoni <36473003+RashmitaSoni@users.noreply.github.com>
2021-03-10 14:47:08 +0530

    Update BatteryAbnormalConditions.cs

--- BatteryAbnormalConditions.cs
+++ BatteryAbnormalConditions.cs
@@ -6,44 +6,19 @@ namespace BatteryCharging
     public class BatteryAbnormalConditions
     {
         static bool IsFaulty = false;
-        static string factorStatusInEnglish, factorStatusInGerman;
-        public static void CheckBreachingPoints(string factorNameInEnglish, string factorNameInGerman, float factorValue, float lowBreachValue,float highBreachValue)
+        public static void CheckBreachingPoints(float factorValue, float lowBreachValue,float highBreachValue)
         {
-            if (factorValue < lowBreachValue)
+            if (factorValue < lowBreachValue || factorValue > highBreachValue)
             {
-                factorStatusInEnglish = English.lowBreachStatus;
-                factorStatusInGerman = German.lowBreachStatus;
                 IsFaulty = true;
-                DisplayAbnormalConditions(factorNameInEnglish, factorNameInGerman, factorStatusInEnglish, factorStatusInGerman);
-            }
-            if(factorValue > highBreachValue)
-            {
-                factorStatusInEnglish = English.highBreachStatus;
-                factorStatusInGerman = German.highBreachStatus;
-                IsFaulty = true;
-                DisplayAbnormalConditions(factorNameInEnglish, factorNameInGerman, factorStatusInEnglish, factorStatusInGerman);
             }
         }
-        public static void CheckWarningPoints(string factorNameInEnglish, string factorNameInGerman,float factorValue, float lowBreachValue, float lowWarningValue, float highWarningValue, float highBreachValue)
+        public static void CheckWarningPoints(float factorValue, float lowBreachValue, float lowWarningValue, float highWarningValue, float highBreachValue)
         {
-            if (factorValue >= lowBreachValue && factorValue <= lowWarningValue)
+            if ((factorValue >= lowBreachValue && factorValue <= lowWarningValue) || (factorValue >= highWarningValue && factorValue <= highBreachValue))
             {
-                factorStatusInEnglish = English.highBreachStatus;
-                factorStatusInGerman = German.highBreachStatus;
                 IsFaulty = false;
-                DisplayAbnormalConditions(factorNameInEnglish,factorNameInGerman,factorStatusInEnglish,factorStatusInGerman);
             }
-            if(factorValue >= highWarningValue && factorValue <= highBreachValue)
-            {
-                factorStatusInEnglish = English.highBreachStatus;
-                factorStatusInGerman = German.highBreachStatus;
-                IsFaulty = false;
-                DisplayAbnormalConditions(factorNameInEnglish, factorNameInGerman, factorStatusInEnglish, factorStatusInGerman);
-            }
-        }
-        public static void DisplayAbnormalConditions(string factorNameInEnglish, string factorNameInGerman, string factorStatusInEnglish, string factorStatusInGerman)
-        {
-            Console.WriteLine(factorNameInEnglish + " " + factorStatusInEnglish + "\n" + factorNameInGerman + " " + factorStatusInGerman);
         }
         public static bool IsBatteryFaultyStatus()
         {
