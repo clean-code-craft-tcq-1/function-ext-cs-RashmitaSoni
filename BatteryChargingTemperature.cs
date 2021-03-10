8a48171e68842341353835796eafc7e8b735275b
RashmitaSoni <36473003+RashmitaSoni@users.noreply.github.com>
2021-03-10 14:47:51 +0530

    Update BatteryChargingTemperature.cs

--- BatteryChargingTemperature.cs
+++ BatteryChargingTemperature.cs
@@ -11,8 +11,8 @@ namespace BatteryCharging
         static float highWarningValue = (float)(highBreachValue - (highBreachValue * 0.05));
         public static void CheckChargingTemperature(float chargingTemperature_Celsius)
         {
-            BatteryAbnormalConditions.CheckBreachingPoints(English.chargingTemperature,German.chargingTemperature,chargingTemperature_Celsius, lowBreachValue, highBreachValue);
-            BatteryAbnormalConditions.CheckWarningPoints(English.chargingTemperature, German.chargingTemperature,chargingTemperature_Celsius, lowBreachValue, lowWarningValue, highWarningValue, highBreachValue);
+            BatteryAbnormalConditions.CheckBreachingPoints(chargingTemperature_Celsius, lowBreachValue, highBreachValue);
+            BatteryAbnormalConditions.CheckWarningPoints(chargingTemperature_Celsius, lowBreachValue, lowWarningValue, highWarningValue, highBreachValue);
         }
     }
 }
