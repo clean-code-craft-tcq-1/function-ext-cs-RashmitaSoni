54f5642667d9d5d5cc3f302b2ed5f2fe0f892871
RashmitaSoni <36473003+RashmitaSoni@users.noreply.github.com>
2021-03-10 21:42:01 +0530

    Update BatteryAbnormalConditions.cs

--- BatteryAbnormalConditions.cs
+++ BatteryAbnormalConditions.cs
@@ -5,25 +5,30 @@ namespace BatteryCharging
 {
     public class BatteryAbnormalConditions
     {
-        static bool IsFaulty = false;
-        public static void CheckBreachingPoints(float factorValue, float lowBreachValue,float highBreachValue)
-        {
-            if (factorValue < lowBreachValue || factorValue > highBreachValue)
+        static readonly string LowBreach = "Breaching Point is Low";
+        static readonly string HighBreach = "Breaching Point is High";
+        static bool FaultyBattery = false;
+        public static void CheckBreachingPoint(string factorName, float factorValue, float minimumFactorLimit, float maximumFactorLimit)
+        { 
+            if (factorValue < minimumFactorLimit)
             {
-                IsFaulty = true;
+                DisplayAbnormalConditions(factorName, minimumFactorLimit, maximumFactorLimit,LowBreach);
+                FaultyBattery = true;
+            }
+            if (factorValue > maximumFactorLimit)
+            {
+                DisplayAbnormalConditions(factorName, minimumFactorLimit, maximumFactorLimit,HighBreach);
+                FaultyBattery = true;
             }
         }
-        public static void CheckWarningPoints(float factorValue, float lowBreachValue, float lowWarningValue, float highWarningValue, float highBreachValue)
+        public static void DisplayAbnormalConditions(string factorName, float minimumFactorLimit, float maximumFactorLimit, string breachPoint)
         {
-            if ((factorValue >= lowBreachValue && factorValue <= lowWarningValue) || (factorValue >= highWarningValue && factorValue <= highBreachValue))
-            {
-                IsFaulty = false;
-            }
+            Console.WriteLine("{0} is out of range!\nRange is ({1},{2})\n{3}",factorName,minimumFactorLimit,maximumFactorLimit,breachPoint);
         }
-        public static bool IsBatteryFaultyStatus()
+        public static bool GetBatteryFaultyStatus()
         {
-            bool isBatteryFaulty = IsFaulty;
-            return isBatteryFaulty;
+            bool faultyBattery = FaultyBattery;
+            return faultyBattery;
         }
     }
 }
