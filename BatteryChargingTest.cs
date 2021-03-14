cc907bdc2afc9da427b59e25588e755c4efc697f
RashmitaSoni <36473003+RashmitaSoni@users.noreply.github.com>
2021-03-14 22:06:25 +0530

    Update BatteryChargingTest.cs

--- BatteryChargingTest.cs
+++ BatteryChargingTest.cs
@@ -5,16 +5,18 @@ namespace BatteryCharging
 {
     public class BatteryChargingTest
     {
+        static bool IsBatteryFaulty;
         public static void BatteryChargingTestFactors(float chargingTemperature_Celsius, float stateOfCharge_percentage, float chargeRate_CRate)
         {
-            BatteryChargingFactors.CheckChargingTemperature(chargingTemperature_Celsius);
-            BatteryChargingFactors.CheckStateOfCharge(stateOfCharge_percentage);
-            BatteryChargingFactors.CheckChargeRate(chargeRate_CRate);
+            IsBatteryFaulty = 
+                BatteryAbnormalConditions.CheckAbnormalConditions(BatteryChargingFactors.ChargingTemperature,chargingTemperature_Celsius,BatteryChargingFactors.minimumChargingTemprature_Celsius,BatteryChargingFactors.minimumChargingTemperatureWarning,BatteryChargingFactors.maximumChargingTemperatureWarning,BatteryChargingFactors.maximumChargingTemprature_Celsius)
+                | BatteryAbnormalConditions.CheckAbnormalConditions(BatteryChargingFactors.StateOfCharge,stateOfCharge_percentage,BatteryChargingFactors.minimumStateOfCharge_Percentage,BatteryChargingFactors.minimumStateOfChargeWarning,BatteryChargingFactors.maximumStateOfChargeWarning,BatteryChargingFactors.maximumStateOfCharge_Percentage)
+                | BatteryAbnormalConditions.CheckAbnormalConditions(BatteryChargingFactors.ChargeRate,chargeRate_CRate,BatteryChargingFactors.minimumChargeRate_CRate,BatteryChargingFactors.minimumChargeRateWarning,BatteryChargingFactors.maximumChargeRate_CRate,BatteryChargingFactors.maximumChargeRate_CRate);
             DisplayBatteryStatus();
         }
         public static void DisplayBatteryStatus()
         {
-            if(BatteryAbnormalConditions.IsBatteryFaultyStatus())
+            if(IsBatteryFaulty)
             {
                 Console.WriteLine("Battery is faulty\nBatterie ist defekt");
             }
