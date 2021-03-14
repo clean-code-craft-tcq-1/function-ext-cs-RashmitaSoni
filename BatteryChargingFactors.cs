58692425df2ee90b1693bc4b25fa128a8337db90
RashmitaSoni <36473003+RashmitaSoni@users.noreply.github.com>
2021-03-14 22:06:42 +0530

    Update BatteryChargingFactors.cs

--- BatteryChargingFactors.cs
+++ BatteryChargingFactors.cs
@@ -5,29 +5,31 @@ namespace BatteryCharging
 {
     public class BatteryChargingFactors
     {
-        static float minimumChargingTemprature_Celsius = 0;
-        static float maximumChargingTemprature_Celsius = 45;
-        static float minimumChargingTemperatureWarning = (float)(minimumChargingTemprature_Celsius + (maximumChargingTemprature_Celsius * 0.05));
-        static float maximumChargingTemperatureWarning = (float)(maximumChargingTemprature_Celsius - (maximumChargingTemprature_Celsius * 0.05));
-        static float minimumStateOfCharge_Percentage = 20;
-        static float maximumStateOfCharge_Percentage = 80;
-        static float minimumStateOfChargeWarning = (float)(minimumStateOfCharge_Percentage + (maximumStateOfCharge_Percentage * 0.05));
-        static float maximumStateOfChargeWarning = (float)(maximumStateOfCharge_Percentage - (maximumStateOfCharge_Percentage * 0.05));
-        static float minimumChargeRate_CRate = 0.3f;
-        static float maximumChargeRate_CRate = 0.8f;
-        static float minimumChargeRateWarning = (float)(minimumChargeRate_CRate + (maximumChargeRate_CRate * 0.05));
-        static float maximumChargeRateWarning = (float)(maximumChargeRate_CRate - (maximumChargeRate_CRate * 0.05));
-        public static void CheckChargingTemperature(float chargingTemperature_Celsius)
-        {
-            BatteryAbnormalConditions.CheckAbnormalConditions("Charging Temperature",chargingTemperature_Celsius, minimumChargingTemprature_Celsius, minimumChargingTemperatureWarning, maximumChargingTemperatureWarning,maximumChargingTemprature_Celsius);
-        }
-        public static void CheckStateOfCharge(float stateOfCharge_percentage)
-        {
-            BatteryAbnormalConditions.CheckAbnormalConditions("State Of Charge",stateOfCharge_percentage, minimumStateOfCharge_Percentage, minimumStateOfChargeWarning, maximumStateOfChargeWarning, maximumStateOfCharge_Percentage);
-        }
-        public static void CheckChargeRate(float chargeRate_CRate)
-        {
-            BatteryAbnormalConditions.CheckAbnormalConditions("Charge Rate",chargeRate_CRate, minimumChargeRate_CRate, minimumChargeRateWarning, maximumChargeRateWarning, maximumChargeRate_CRate);
-        }
+        public static readonly string ChargingTemperature = "Charging Temperature";
+        public static readonly string StateOfCharge = "State of Charge";
+        public static readonly string ChargeRate = "Charge Rate";
+        public static float minimumChargingTemprature_Celsius = 0;
+        public static float maximumChargingTemprature_Celsius = 45;
+        public static float minimumChargingTemperatureWarning = (float)(minimumChargingTemprature_Celsius + (maximumChargingTemprature_Celsius * 0.05));
+        public static float maximumChargingTemperatureWarning = (float)(maximumChargingTemprature_Celsius - (maximumChargingTemprature_Celsius * 0.05));
+        public static float minimumStateOfCharge_Percentage = 20;
+        public static float maximumStateOfCharge_Percentage = 80;
+        public static float minimumStateOfChargeWarning = (float)(minimumStateOfCharge_Percentage + (maximumStateOfCharge_Percentage * 0.05));
+        public static float maximumStateOfChargeWarning = (float)(maximumStateOfCharge_Percentage - (maximumStateOfCharge_Percentage * 0.05));
+        public static float minimumChargeRate_CRate = 0.3f;
+        public static float maximumChargeRate_CRate = 0.8f;
+        public static float minimumChargeRateWarning = (float)(minimumChargeRate_CRate + (maximumChargeRate_CRate * 0.05));
+        public static float maximumChargeRateWarning = (float)(maximumChargeRate_CRate - (maximumChargeRate_CRate * 0.05));
+    }
+    public class Messages
+    {
+        public static string lowBreachMessageInEnglish = "Breaching point is low";
+        public static string lowBreachMessageInGerman = "Der Bruchpunkt ist niedrig";
+        public static string highBreachMessageInEnglish = "Breaching point is high";
+        public static string highBreachMessageInGerman = "Der Bruchpunkt ist hoch";
+        public static string lowWarningMessageInEnglish = "Warning: Approaching discharge";
+        public static string lowWarningMessageInGerman = "Warnung: Annäherung an die Entladung";
+        public static string highWarningMessageInEnglish = "Warning: Approaching charge-peak";
+        public static string highWarningMessageInGerman = "Warnung: Annäherung an die Ladungsspitze";
     }
 }
