d4679235e3fd3176cc81cc6e30b819c34cad343b
RashmitaSoni <36473003+RashmitaSoni@users.noreply.github.com>
2021-03-17 23:39:18 +0530

    Update BatteryChargingTest.cs

--- BatteryChargingTest.cs
+++ BatteryChargingTest.cs
@@ -5,26 +5,22 @@ namespace BatteryCharging
 {
     public class BatteryChargingTest
     {
-        static bool IsBatteryFaulty;
+        static IReporter _batteryReporter;
+        public BatteryChargingTest(IReporter batteryReporter)
+        {
+            if (batteryReporter == null)
+            {
+                throw new InvalidProgramException("Invalid Program Exception Occurred !!!");
+            }
+            _batteryReporter = batteryReporter;
+        }
         public static void BatteryChargingTestFactors(float chargingTemperature_Celsius, float stateOfCharge_percentage, float chargeRate_CRate)
         {
-            IsBatteryFaulty = 
-                BatteryAbnormalConditions.CheckAbnormalConditions(BatteryChargingFactors.ChargingTemperature,chargingTemperature_Celsius,BatteryChargingFactors.minimumChargingTemprature_Celsius,BatteryChargingFactors.minimumChargingTemperatureWarning,BatteryChargingFactors.maximumChargingTemperatureWarning,BatteryChargingFactors.maximumChargingTemprature_Celsius)
-                | BatteryAbnormalConditions.CheckAbnormalConditions(BatteryChargingFactors.StateOfCharge,stateOfCharge_percentage,BatteryChargingFactors.minimumStateOfCharge_Percentage,BatteryChargingFactors.minimumStateOfChargeWarning,BatteryChargingFactors.maximumStateOfChargeWarning,BatteryChargingFactors.maximumStateOfCharge_Percentage)
-                | BatteryAbnormalConditions.CheckAbnormalConditions(BatteryChargingFactors.ChargeRate,chargeRate_CRate,BatteryChargingFactors.minimumChargeRate_CRate,BatteryChargingFactors.minimumChargeRateWarning,BatteryChargingFactors.maximumChargeRate_CRate,BatteryChargingFactors.maximumChargeRate_CRate);
-            DisplayBatteryStatus();
+            bool IsBatteryFaulty =
+                BatteryAbnormalConditions.CheckAbnormalConditions(BatteryChargingFactors.ChargingTemperature, chargingTemperature_Celsius, BatteryChargingFactors.minimumChargingTemprature_Celsius, BatteryChargingFactors.minimumChargingTemperatureWarning, BatteryChargingFactors.maximumChargingTemperatureWarning, BatteryChargingFactors.maximumChargingTemprature_Celsius)
+                | BatteryAbnormalConditions.CheckAbnormalConditions(BatteryChargingFactors.StateOfCharge, stateOfCharge_percentage, BatteryChargingFactors.minimumStateOfCharge_Percentage, BatteryChargingFactors.minimumStateOfChargeWarning, BatteryChargingFactors.maximumStateOfChargeWarning, BatteryChargingFactors.maximumStateOfCharge_Percentage)
+                | BatteryAbnormalConditions.CheckAbnormalConditions(BatteryChargingFactors.ChargeRate, chargeRate_CRate, BatteryChargingFactors.minimumChargeRate_CRate, BatteryChargingFactors.minimumChargeRateWarning, BatteryChargingFactors.maximumChargeRate_CRate, BatteryChargingFactors.maximumChargeRate_CRate);
+            _batteryReporter.DisplayBatteryStatus(IsBatteryFaulty, BatteryAbnormalConditions.factorsConditions);
         }
-        public static void DisplayBatteryStatus()
-        {
-            if(IsBatteryFaulty)
-            {
-                Console.WriteLine("Battery is faulty\nBatterie ist defekt");
-            }
-            else
-            {
-                Console.WriteLine("Battery is OK\nBatterie ist in Ordnung");
-            }
-           
-        }
-    }
+   }
 }
