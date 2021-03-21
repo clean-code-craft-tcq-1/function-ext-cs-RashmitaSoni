9750c67576bf77f42d7d7ffe05a18e500bf1cb2a
RashmitaSoni <36473003+RashmitaSoni@users.noreply.github.com>
2021-03-21 22:54:26 +0530

    Update BatteryChargingFactors.cs

--- BatteryChargingFactors.cs
+++ BatteryChargingFactors.cs
@@ -21,18 +21,4 @@ namespace BatteryCharging
         public static float minimumChargeRateWarning = (float)(minimumChargeRate_CRate + (maximumChargeRate_CRate * 0.05));
         public static float maximumChargeRateWarning = (float)(maximumChargeRate_CRate - (maximumChargeRate_CRate * 0.05));
     }
-    public class GermanLanguage
-    {
-        public static string lowBreachMessage = "Der Bruchpunkt ist niedrig";
-        public static string highBreachMessage = "Der Bruchpunkt ist hoch";
-        public static string lowWarningMessage = "Warnung: Annäherung an die Entladung";
-        public static string highWarningMessage = "Warnung: Annäherung an die Ladungsspitze";
-    }
-    public class EnglishLanguage
-    {
-        public static string lowBreachMessage = "Breaching point is low";
-        public static string highBreachMessage = "Breaching point is high";
-        public static string lowWarningMessage = "Warning: Approaching discharge";
-        public static string highWarningMessage = "Warning: Approaching charge-peak";
-    }
 }
