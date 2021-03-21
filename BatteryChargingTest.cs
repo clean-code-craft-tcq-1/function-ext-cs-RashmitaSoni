4606ae843edea255dd4310c9a5d1df7bb5d1e1f0
RashmitaSoni <36473003+RashmitaSoni@users.noreply.github.com>
2021-03-21 22:54:46 +0530

    Update BatteryChargingTest.cs

--- BatteryChargingTest.cs
+++ BatteryChargingTest.cs
@@ -14,23 +14,32 @@ namespace BatteryCharging
             }
             _batteryReporter = batteryReporter;
         }
-        public static void BatteryChargingTestFactors(float chargingTemperature_Celsius, float stateOfCharge_percentage, float chargeRate_CRate)
+        public static bool BatteryChargingTestFactors(float chargingTemperature_Celsius, float stateOfCharge_percentage, float chargeRate_CRate)
         {
-            bool IsBatteryFaulty =
-                BatteryAbnormalConditions.CheckAbnormalConditions(BatteryChargingFactors.ChargingTemperature, chargingTemperature_Celsius, BatteryChargingFactors.minimumChargingTemprature_Celsius, BatteryChargingFactors.minimumChargingTemperatureWarning, BatteryChargingFactors.maximumChargingTemperatureWarning, BatteryChargingFactors.maximumChargingTemprature_Celsius)
-                | BatteryAbnormalConditions.CheckAbnormalConditions(BatteryChargingFactors.StateOfCharge, stateOfCharge_percentage, BatteryChargingFactors.minimumStateOfCharge_Percentage, BatteryChargingFactors.minimumStateOfChargeWarning, BatteryChargingFactors.maximumStateOfChargeWarning, BatteryChargingFactors.maximumStateOfCharge_Percentage)
-                | BatteryAbnormalConditions.CheckAbnormalConditions(BatteryChargingFactors.ChargeRate, chargeRate_CRate, BatteryChargingFactors.minimumChargeRate_CRate, BatteryChargingFactors.minimumChargeRateWarning, BatteryChargingFactors.maximumChargeRate_CRate, BatteryChargingFactors.maximumChargeRate_CRate);
-
-            if (IsBatteryFaulty)
+            try
             {
-                BatteryAbnormalConditions.factorsConditions.Add(new Tuple<string, string>("Battery is faulty", "Batterie ist defekt"));
-            }
-            else
-            {
-                BatteryAbnormalConditions.factorsConditions.Add(new Tuple<string, string>("Battery is OK", "Batterie ist in Ordnung"));
-            }
+                bool IsBatteryFaulty =
+                    BatteryAbnormalConditions.CheckAbnormalConditions(BatteryChargingFactors.ChargingTemperature, chargingTemperature_Celsius, BatteryChargingFactors.minimumChargingTemprature_Celsius, BatteryChargingFactors.minimumChargingTemperatureWarning, BatteryChargingFactors.maximumChargingTemperatureWarning, BatteryChargingFactors.maximumChargingTemprature_Celsius)
+                    | BatteryAbnormalConditions.CheckAbnormalConditions(BatteryChargingFactors.StateOfCharge, stateOfCharge_percentage, BatteryChargingFactors.minimumStateOfCharge_Percentage, BatteryChargingFactors.minimumStateOfChargeWarning, BatteryChargingFactors.maximumStateOfChargeWarning, BatteryChargingFactors.maximumStateOfCharge_Percentage)
+                    | BatteryAbnormalConditions.CheckAbnormalConditions(BatteryChargingFactors.ChargeRate, chargeRate_CRate, BatteryChargingFactors.minimumChargeRate_CRate, BatteryChargingFactors.minimumChargeRateWarning, BatteryChargingFactors.maximumChargeRate_CRate, BatteryChargingFactors.maximumChargeRate_CRate);
 
-            _batteryReporter.DisplayBatteryStatus(BatteryAbnormalConditions.factorsConditions);
+                if (IsBatteryFaulty)
+                {
+                    BatteryAbnormalConditions.factorsConditions.Add(new Tuple<string, string>("Battery is faulty", "Batterie ist defekt"));
+                }
+                else
+                {
+                    BatteryAbnormalConditions.factorsConditions.Add(new Tuple<string, string>("Battery is OK", "Batterie ist in Ordnung"));
+                }
+
+                _batteryReporter.DisplayBatteryStatus(BatteryAbnormalConditions.factorsConditions);
+                return true;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+                return false;
+            }
         }
    }
 }
